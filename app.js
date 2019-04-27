97ce87f701c0df37c44712e1faa1b2e5c8f09f4e
林玉堂 <>
2019-04-27 09:52:06 -0700

    add route

--- app.js
+++ app.js
@@ -17,9 +17,43 @@ db.once('open', ()=>{
 
 //    載入 todo model
 const Todo = require('./models/todo');
+// Todo 首頁
 app.get('/',(req, res)=>{
   res.send('hello world!')
 })
+//列出全部 Todo
+app.get('/todos',(req, res)=>{
+  res.send('列出所有的todo')
+})
+//新增一筆 Todo 頁面
+app.get('/todos/new', (req, res) => {
+  res.send('新增todo')
+})
+//新增一筆 Todo
+app.post('/todos', (req, res) => {
+  res.send('建立todo')
+})
+
+
+//顯示 Todo 詳細內容
+app.get('/todos/:id', (req, res) => {
+  res.send('顯示todo')
+})
+
+//修改Todo 頁面
+app.get('/todos/：id/edit', (req, res) => {
+  res.send('修改todo')
+})
+
+app.post('/todos/:id', (req, res) => {
+  res.send('修改todo')
+})
+
+//刪除Todo
+app.post('/todos/：id/delete', (req, res) => {
+  res.send('刪除todo')
+})
+
 
 app.listen(3000, ()=>{
   console.log('app is runing')
